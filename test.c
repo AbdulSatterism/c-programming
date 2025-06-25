a7b16424b1115b8220ca8a777cd135d556cb4f6f
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-06-25 21:09:45 +0600

    changes file name index to test

---
+++ test.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//* all variable declarations must be at the beginning of a block
+// This is a simple C program that prints "Hello, World!" to the console.
+// The main function is the entry point of the program.
+
+
+
+
+int main() {
+    // Print a message to the console
+    printf("Hello, World!\n");
+    
+    // Return 0 to indicate successful execution
+    return 0;
+}
